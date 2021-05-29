d550b6ca8742b01d89ac5ff2f1e658c8a975027c
Nannan <nanna7077@gmail.com>
2021-05-29 11:11:41 +0000

    Fix . to ,

--- static/randomProblems_Home.js
+++ static/randomProblems_Home.js
@@ -3,7 +3,7 @@ function workwithdata(data) {
 };
 
 $.ajax({
-    url: "notebookofproblems/static/problemList.json".
+    url: "notebookofproblems/static/problemList.json",
     success: function (data) {
     workwithdata(data);
     },
