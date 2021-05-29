b60f36e9a66e9fba437b4351f400668d03f9dd99
Nannan <nanna7077@gmail.com>
2021-05-29 10:45:45 +0000

    Update randomProblems_Home.js

--- static/randomProblems_Home.js
+++ static/randomProblems_Home.js
@@ -1,16 +1,10 @@
-function loadJSON(callback) {
-    var xObj = new XMLHttpRequest();
-    xObj.overrideMimeType("application/json");
-    xObj.open('GET', '/static/problemList.json', true);
-    xObj.onreadystatechange = function() {
-        if (xObj.readyState === 4 && xObj.status === 200) {
-            callback(xObj.responseText);
-        }
-    };
-    xObj.send(null);
-  }
-
-loadJSON(function(response) {
-    var data = JSON.parse(response);
+function workwithdata(data) {
+    var data = JSON.parse();
     console.log(data);
-});
+};
+
+fetch("/static/problemList.json").
+then(response => {
+    return response.json();
+})
+.then(data => workwithdata(data));
