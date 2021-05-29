4234d69e17cb7977d9dba98312d8728b50427ca7
Nannan <nanna7077@gmail.com>
2021-05-29 11:44:16 +0000

    Removed debug logs

--- static/randomProblems_Home.js
+++ static/randomProblems_Home.js
@@ -1,13 +1,10 @@
 function workwithdata(data) {
     var final_array=[];
-    console.log(data);
     final_array.push(data['problems'][Math.floor(Math.random() * data['problems'].length)]);
     final_array.push(data['problems'][Math.floor(Math.random() * data['problems'].length)]);
     final_array.push(data['problems'][Math.floor(Math.random() * data['problems'].length)]);
     final_array.push(data['problems'][Math.floor(Math.random() * data['problems'].length)]);
     final_array.push(data['problems'][Math.floor(Math.random() * data['problems'].length)]);
-    console.log("---");
-    console.log(final_array);
     var container_ = document.getElementById("randomproblems");
     container_.innerHTML='';
     for (i in final_array) {
