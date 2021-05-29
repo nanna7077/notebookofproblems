16d79f5b4aa6f91ec9d6ecf3d16dee8fd629a1e3
Nannan <nanna7077@gmail.com>
2021-05-29 17:46:18 +0000

    Create solution_ruby_nannan.rb

---
+++ problems/range_from_list/solution_ruby_nannan.rb
@@ -0,0 +1,28 @@
+a=gets.chomp
+b=gets.chomp
+c=gets.chomp
+d=gets.chomp
+e=gets.chomp
+print "["
+for i in a..b
+print " #{i} "
+end
+print "]"
+print "\n"
+print "["
+for i in b..c
+print " #{i} "
+end
+print "]"
+print "\n"
+print "["
+for i in c..d
+print " #{i} "
+end
+print "]"
+print "\n"
+print "["
+for i in d..e
+print " #{i} "
+end
+print "]"
