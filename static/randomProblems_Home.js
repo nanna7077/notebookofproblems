c324fa2b1a33ef659e617fdae9e50ee69cadbe88
Nannan <nanna7077@gmail.com>
2021-05-29 10:56:08 +0000

    changed var json to data

--- static/randomProblems_Home.js
+++ static/randomProblems_Home.js
@@ -2,4 +2,4 @@ function workwithdata(data) {
     console.log(data);
 };
 
-$.getJSON("notebookofproblems/static/problemList.json", workwithdata(json));
+$.getJSON("notebookofproblems/static/problemList.json", workwithdata(data));
