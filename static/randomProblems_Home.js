a083259ecec0a374e80092af02a49f4a42c71e63
Nannan <nanna7077@gmail.com>
2021-05-29 11:09:22 +0000

    switch to ajax

--- static/randomProblems_Home.js
+++ static/randomProblems_Home.js
@@ -2,4 +2,13 @@ function workwithdata(data) {
     console.log(data);
 };
 
-$.getJSON("notebookofproblems/static/problemList.json", function(data) { workwithdata(data); });
+$.ajax({
+    url: "notebookofproblems/static/problemList.json".
+    success: function (data) {
+    workwithdata(data);
+    },
+    error: function(data) {
+        console.log("Couldn't fetch json");
+    },
+    dataType: "json",
+    timeout: 3000});
